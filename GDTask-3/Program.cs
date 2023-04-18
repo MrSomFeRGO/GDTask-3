71129f1a1d489252868b0828d199747e120892bc
MrSomFeRGO <mr.somfergo@gmail.com>
2023-04-18 17:34:15 +0300

    1

--- GDTask-3/Program.cs
+++ GDTask-3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(shifr("test", 1700));
+
+static string shifr(string toShifr, int i)
+{
+    string result = "";
+    foreach (var element in toShifr)
+    {
+        int el_char = (int)element + i;
+        el_char = recursiya22(el_char);
+
+        result += (char)el_char;
+    }
+
+    return result;
+}
+
+static int recursiya22(int i)
+{
+    if (i > 255)
+    {
+        i -= 255;
+    }
+
+    if (i > 255)
+    {
+        return recursiya22(i);
+    }
+
+    return i;
+}
